f5af3eb3d66dfc95ab66ec4665d25c291bd14380
LeserraJ <leserrabusiness@gmail.com>
2023-10-17 04:00:20 +0000

    third commit

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -1,5 +1,18 @@
 import React from "react";
 
+fetch("https://swapi.dev/api/people/")
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => {
+    data = JSON.parse(data);
+    data.forEach((person) => {
+      const markup = <h5>${person.name}</h5>;
+    });
+    console.log(data);
+  })
+  .catch((error) => console.log(error));
+
 export const Card = (props) => {
   <div className="card">
     <img src="..." className="card-img-top" alt="..." />
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Card } from "../component/card.js";
 import "../../styles/home.css";
 
+
+
 export const Home = () => (
   <div className="container-fluid" id="main_container">
     <div className="row">
