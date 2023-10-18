1b5ea340b29098889d54f73b12516bf94ea7a5bc
LeserraJ <leserrabusiness@gmail.com>
2023-10-18 02:11:30 +0000

    added some scroll

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -11,7 +11,7 @@ export const Card = () => {
     const response = await fetch(url);
     try {
       const responseJson = await response.json();
-      const data = (responseJson.results);
+      const data = responseJson.results;
       setCardData(data);
     } catch (err) {
       console.error(err);
@@ -23,32 +23,27 @@ export const Card = () => {
   }, []);
 
   return (
-    <>   
-    {cardData.map((card)=>{
-      return(
-        <div className="card">
-      <img src="..." className="card-img-top" alt="..." />
-      <div className="card-body">
-      <h5 className="card- title">{card.name}</h5>
-        <p className="card-text">
-          {card.height} , {card.mass}
-        </p>
-        <a href="#" className="btn btn-primary">
-          {card.url}</a>
-      </div>
-      </div>
-       
-      );
-    })}
-
+    <>
+      {cardData.map((card) => {
+        return (
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card- title">{card.name}</h5>
+              <p className="card-text">
+                Height:{card.height}  Weight:{card.mass}
+              </p>
+              <a href="#" className="btn btn-primary">
+                {card.url}
+              </a>
+            </div>
+          </div>
+        );
+      })}
     </>
-
-  )
+  );
 };
 
-
-
-    /*return(<div className="card">
+/*return(<div className="card">
       <img src="..." className="card-img-top" alt="..." />
       <div className="card-body">
         <h5 className="card- title">{card.name}</h5>
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,13 +1,14 @@
 import React from "react";
-import{ Card }from "../component/card.js";
+import { Card } from "../component/card.js";
 import "../../styles/home.css";
-
-
 
 export const Home = () => (
   <div className="container-fluid" id="main_container">
-    <div className="row">
-      <Card />
+    <div>
+      <h2>People</h2>
+      <div className="col">
+        <Card id="card" />
+      </div>
     </div>
   </div>
 );
